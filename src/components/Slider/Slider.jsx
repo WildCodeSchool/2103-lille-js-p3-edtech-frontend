5ce8b595c31809c41653d9a66e0a38792f08fd4c
yulen59 <j.delemarle@gmail.com>
2021-06-02 16:58:26 +0200

    create slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,3 +1,85 @@
-export default function Slider() {
-  return <div>Slider</div>;
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import PropTypes from 'prop-types';
+import Sslider from './Style';
+
+const slideImages = [
+  './img/slider/bureau1.jpeg',
+  './img/slider/bureau2.jpeg',
+  './img/slider/bureau3.jpeg',
+];
+
+const properties = {
+  duration: 3500,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrow: true,
+};
+
+function Slider({ theme }) {
+  return (
+    <Sslider className="slide-container" theme={theme}>
+      <Slide {...properties}>
+        <div className="each-slide">
+          <div
+            className="each-img"
+            style={{ backgroundImage: `url(${slideImages[0]})` }}
+          >
+            <span className="each-text">
+              <h2 className="each-title">
+                Mettre la technologie au service de l&apos;éducation et de la
+                formation
+              </h2>
+              <p className="slider-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </span>
+          </div>
+        </div>
+        <div className="each-slide">
+          <div style={{ backgroundImage: `url(${slideImages[1]})` }}>
+            <span className="each-text">
+              <h2 className="each-title">
+                Mettre la technologie au service de l&apos;éducation et de la
+                formation
+              </h2>
+              <p className="slider-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </span>
+          </div>
+        </div>
+        <div className="each-slide">
+          <div style={{ backgroundImage: `url(${slideImages[2]})` }}>
+            <span className="each-text">
+              <h2 className="each-title">
+                Mettre la technologie au service de l&apos;éducation et de la
+                formation
+              </h2>
+              <p className="slider-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </span>
+          </div>
+        </div>
+      </Slide>
+    </Sslider>
+  );
 }
+
+Slider.propTypes = {
+  theme: PropTypes.string.isRequired,
+};
+
+export default Slider;
---
+++ src/components/Slider/Style.jsx
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+const Sslider = styled.div`
+  width: 100vw;
+  padding-top: 20px;
+  margin: auto;
+
+  .each-slide > div {
+    display: flex;
+    align-items: center;
+    justify-content: left;
+
+    background-size: cover;
+    height: 350px;
+  }
+
+  .each-slide span {
+    padding: 20px;
+    font-size: 1.1em;
+    background-color: rgba(55, 106, 182, 0.664);
+    text-align: center;
+    border-radius: 5px;
+    width: 50%;
+    height: 100%;
+  }
+  .each-img {
+    height: 75vh;
+    width: 100%;
+  }
+
+  .each-text {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: justify;
+    justify-content: center;
+    color: white;
+  }
+
+  .slider-text {
+    display: flex;
+
+    margin-left: 300px;
+    text-align: justify;
+  }
+  .each-title {
+    display: flex;
+    font-size: 1.2em;
+    margin-left: 300px;
+    text-align: justify;
+  }
+  /* .each-slideshow-indicator::before {
+    background: ${({ theme }) => theme.fontColor || 'yellowgreen'};
+  }
+
+  .style {
+    height: 75vh;
+    width: 100%;
+  } 
+  */
+`;
+
+export default Sslider;
