d14dfc28ad0ae5d0858df13c76574c1f00cfbc26
Georges FORTRY <gfortry@hotmail.com>
2021-06-05 00:28:22 +0200

    fonctionnal members carousel

---
+++ src/components/Members/Style.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const SMembers = styled.div`
+  .Carousel {
+    margin: auto;
+    width: 100vw;
+    height: 20vh;
+    .slide {
+      height: 15vh;
+
+      img {
+        display: block;
+        height: 100%;
+
+        margin: auto;
+      }
+    }
+  }
+`;
+export default SMembers;
