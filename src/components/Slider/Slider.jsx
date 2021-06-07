adb40c09d16d48e7e0ee9561ed585bca6f232e2e
yulen59 <j.delemarle@gmail.com>
2021-06-07 22:05:40 +0200

    create backend routes for slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,43 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import dotenv from 'dotenv';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
 import Sslider from './Style';
 
-const slideImages = [
-  './img/slider/bureau1.jpeg',
-  './img/slider/bureau2.jpeg',
-  './img/slider/bureau3.jpeg',
-];
 const properties = {
   duration: 3500,
   transitionDuration: 500,
   infinite: true,
   indicators: true,
-  arrow: true,
+  arrow: 'true',
 };
 
+dotenv.config();
+
 function Slider() {
+  const [sliders, setSliders] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/sliders`).then(({ data }) => {
+      setSliders(data);
+    });
+  }, []);
+
   return (
     <Sslider className="slide-container">
       <Slide {...properties}>
-        {slideImages.map((image) => {
+        {sliders.map((slide) => {
           return (
             <div className="each-slide">
-              <div
-                className="each-img"
-                style={{ backgroundImage: `url(${image})` }}
-              >
+              <div className="each-img">
+                <img src={slide.img_src} alt={slide.img_alt} />
                 <span className="each-text">
-                  <h2 className="each-title">
-                    Mettre la technologie au service de l&apos;éducation et de
-                    la formation
-                  </h2>
-                  <p className="slider-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </p>
+                  <h2 className="each-title">src={slide.title}</h2>
+                  <p className="slider-text">src={slide.text}</p>
                 </span>
               </div>
             </div>
