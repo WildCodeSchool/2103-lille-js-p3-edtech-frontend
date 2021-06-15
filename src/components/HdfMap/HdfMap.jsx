5bd9fa7c2b3c61ccd13aba3c1ea276a4171486c0
TB <thaibang1983hn@gmail.com>
2021-06-15 14:48:58 +0200

    map modified

--- src/components/HdfMap/HdfMap.jsx
+++ src/components/HdfMap/HdfMap.jsx
@@ -16,5 +16,32 @@ export default function HdfMap() {
       });
   }, []);
 
-  return <SMap className="map">{externeLinks?.link_map}</SMap>;
+  return (
+    <SMap className="map">
+      <h1 className="title-map">
+        Cartographie représent les members de l&apos;asociation
+      </h1>
+      <p className="description-map">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus
+        ab iure reiciendis at! Iure, libero error quae repellat similique
+        officia beatae doloremque praesentium, quod nobis quaerat voluptatum
+        autem, magnam debitis? Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Delectus suscipit, non error laboriosam eos animi quas
+        dolores? Voluptates voluptatem beatae, praesentium sed esse porro
+        explicabo reprehenderit deserunt culpa, laudantium fugit.
+      </p>
+
+      <button type="button" className="btn-map">
+        <a
+          className="link-map"
+          href="https://livemap.getwemap.com/embed.html?emmid=15883&iframeid=ed23008b-869a-482f-8f70-21c1041b88ed&parentLocation=https%3A%2F%2Flivemap.getwemap.com%2Fembed.html&parentOrigin=https%3A%2F%2Flivemap.getwemap.com&token=CQW96JV4PEI60Z56A7MBB7G2X#/search@48.8526893,2.3282879,5.00"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Lien à cartographie
+        </a>
+      </button>
+      {externeLinks?.link_map}
+    </SMap>
+  );
 }
