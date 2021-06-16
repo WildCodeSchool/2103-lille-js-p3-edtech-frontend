394f05054d9c689f06a4b2f817d5c599510c7adf
yulen59 <j.delemarle@gmail.com>
2021-06-16 16:04:11 +0200

    update footer component and style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import SFooter from './Style';
 
 export default function Footer() {
   const [translations, setTranslations] = useState({});
-  const [externeLinks, SetExterneLinks] = useState({});
+
   const [images, setImages] = useState({});
 
   useEffect(() => {
@@ -15,12 +15,6 @@ export default function Footer() {
     axios.get(`${process.env.REACT_APP_API_URL}/images`).then(({ data }) => {
       setImages(data);
     });
-
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/external_links`)
-      .then(({ data }) => {
-        SetExterneLinks(data);
-      });
   }, []);
 
   return (
@@ -32,20 +26,30 @@ export default function Footer() {
     >
       <div className="links">
         <ul>
-          <a href="#Actualités">
-            <li className="list">{translations.first_btn || null}</li>
+          <a href="#map">
+            <li className="list">{translations.first_section || null}</li>
           </a>
-          <a
-            href={externeLinks?.link_helloasso}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <li className="list">{translations.second_btn || null}</li>
+          <a href="#Actualités">
+            <li className="list">{translations.fourth_section || null}</li>
+          </a>
+          <a href="#members">
+            <li className="list">{translations.second_section || null}</li>
+          </a>
+          <a href="#partners">
+            <li className="list">{translations.third_section || null}</li>
           </a>
           <a href="#Contact">
-            <li className="list">{translations.third_btn || null}</li>
+            <li className="list">{translations.fifth_section || null}</li>
           </a>
         </ul>
+        <div className="date">{translations.date || null}</div>
+      </div>
+
+      <div className="infos">
+        <div>
+          <h3>Edtech Hauts-de-France:</h3>
+          <p>165 Avenue de Bretagne 59000 Lille </p>
+        </div>
       </div>
       <div className="copyright">
         <a
--- src/components/Footer/Style.jsx
+++ src/components/Footer/Style.jsx
@@ -4,17 +4,25 @@ const SFooter = styled.footer`
   bottom: 0;
   left: 0;
   right: 0;
-  height: 10vh;
-  font-size: 1em;
+  height: 17vh;
+  font-size: 0.8em;
   padding: 20px;
   display: flex;
   justify-content: flex-end;
   background-size: cover;
 
-  .links {
-    font-size: 1em;
-    margin-right: 10%;
+  .infos {
+    color: white;
+    font-size: 0.9em;
     line-height: 1.5em;
+  }
+  .links {
+    font-size: 0.9em;
+    line-height: 1.5em;
+    margin-right: 15%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
 
   .links a {
@@ -25,13 +33,22 @@ const SFooter = styled.footer`
   .copyright {
     text-align: right;
     line-height: 1.5em;
-    width: 30vw;
+    width: 15vw;
     color: white;
+    display: flex;
+    flex-direction: column-reverse;
+    margin-bottom: 20px;
   }
 
   .copyright a {
     color: white;
     text-decoration: none;
+    font-size: 0.9em;
+  }
+
+  .date {
+    color: white;
+    font-size: 0.9em;
   }
 `;
 
