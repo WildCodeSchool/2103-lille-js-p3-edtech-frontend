c2fac7a56f75ad70ec185c390fad03ba59ef7d19
Georges FORTRY <gfortry@hotmail.com>
2021-06-21 16:36:59 +0200

    editable section order

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import dotenv from 'dotenv';
 import Header from './components/Header/Header';
 import Slider from './components/Slider/Slider';
 import NavBar from './components/NavBar/NavBar';
@@ -9,21 +12,29 @@ import HdfTwitter from './components/HdfTwitter/HdfTwitter';
 import ContactForm from './components/ContactForm/ContactForm';
 import Footer from './components/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <NavBar />
-      <Slider />
-      <HdfMap />
-      <Members />
-      <Partners />
-      <Events />
-      <HdfTwitter />
-      <ContactForm />
-      <Footer />
-    </div>
-  );
-}
+dotenv.config();
 
-export default App;
+export default function App() {
+  const [sections, setSections] = useState([]);
+  let nameSection = null;
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/sections`).then(({ data }) => {
+      const tmpSections = data.map((section) => {
+        if (section.name === 'Header') nameSection = <Header />;
+        if (section.name === 'NavBar') nameSection = <NavBar />;
+        if (section.name === 'Slider') nameSection = <Slider />;
+        if (section.name === 'HdfMap') nameSection = <HdfMap />;
+        if (section.name === 'Members') nameSection = <Members />;
+        if (section.name === 'Partners') nameSection = <Partners />;
+        if (section.name === 'Events') nameSection = <Events />;
+        if (section.name === 'HdfTwitter') nameSection = <HdfTwitter />;
+        if (section.name === 'ContactForm') nameSection = <ContactForm />;
+        if (section.name === 'Footer') nameSection = <Footer />;
+        return nameSection;
+      });
+      setSections(tmpSections);
+    });
+  }, []);
+  return <div className="App">{sections}</div>;
+}
