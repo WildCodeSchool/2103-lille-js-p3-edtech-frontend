e75c06319be3c14c0f98e74f953ce6829f864dd2
TB <thaibang1983hn@gmail.com>
2021-06-23 16:37:33 +0200

    actuTwit

--- src/components/HdfTwitter/HdfTwitter.jsx
+++ src/components/HdfTwitter/HdfTwitter.jsx
@@ -6,18 +6,18 @@ import SActuTwit from './Style';
 dotenv.config();
 
 export default function HdfTwitter() {
-  const [images, setImages] = useState({});
+  const [actus, setActus] = useState({});
   const [translations, setTranslations] = useState({});
-  const [externeLinks, SetExterneLinks] = useState({});
+  const [externalLinks, SetExternalLinks] = useState({});
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_URL}/images`).then(({ data }) => {
-      setImages(data);
+    axios.get(`${process.env.REACT_APP_API_URL}/actus`).then(({ data }) => {
+      setActus(data);
     });
     axios
       .get(`${process.env.REACT_APP_API_URL}/external_links`)
       .then(({ data }) => {
-        SetExterneLinks(data);
+        SetExternalLinks(data);
       });
     axios.get(`${process.env.REACT_APP_API_URL}/texts`).then(({ data }) => {
       setTranslations(data);
@@ -25,74 +25,151 @@ export default function HdfTwitter() {
   }, []);
 
   return (
-    <SActuTwit className="ActuTwit">
-      <div className="twoActus">
-        <div className="actu1">
-          <div>
-            <h1 className="title-actu1">{translations.title_actu1 || null}</h1>
-            <p className="content-actu1">
-              {translations.content_actu1 || null}
-            </p>
-            <p>
-              <a
-                className="link-actu1"
-                href={externeLinks?.link_actu1}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Lien vers actu1
-              </a>
-            </p>
+    <SActuTwit id="Actualités">
+      <h1 id="actualites">{translations.fourth_section || null}</h1>
+      <div className="ActuTwit">
+        <div className="twoActus-twit twoActus ">
+          <div className="actu1">
+            <div>
+              <h1 className="title-actu">{translations.title_actu1 || null}</h1>
+              <p className="content-actu1">
+                {translations.content_actu1 || null}
+              </p>
+              <p>
+                <a
+                  className="link-actu"
+                  href={externalLinks?.link_actu1}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Lien vers actu1
+                </a>
+              </p>
+            </div>
+            <div>
+              <img
+                className="photo_actu"
+                src={actus?.photo_actu1?.src}
+                alt={actus?.photo_actu1?.alt}
+              />
+            </div>
           </div>
-          <div>
-            <img
-              className="photo_actu"
-              src={images?.photo_actu1?.src}
-              alt={images?.photo_actu1?.alt}
-            />
+          <div className="actu2">
+            <div>
+              <h1 className="title-actu">{translations.title_actu2 || null}</h1>
+              <p className="content-actu2">
+                {translations.content_actu2 || null}
+              </p>
+              <p>
+                <a
+                  className="link-actu"
+                  href={externalLinks?.link_actu2}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Lien vers actu2
+                </a>
+              </p>
+            </div>
+            <div>
+              <img
+                className="photo_actu"
+                src={actus?.photo_actu2?.src}
+                alt={actus?.photo_actu2?.img_alt}
+              />
+            </div>
+          </div>
+          <div className="actu3">
+            <div>
+              <h1 className="title-actu">{translations.title_actu3 || null}</h1>
+              <p className="content-actu3">
+                {translations.content_actu3 || null}
+              </p>
+              <p>
+                <a
+                  className="link-actu"
+                  href={externalLinks?.link_actu3}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Lien vers actu3
+                </a>
+              </p>
+            </div>
+            <div>
+              <img
+                className="photo_actu"
+                src={actus?.photo_actu3?.src}
+                alt={actus?.photo_actu3?.alt}
+              />
+            </div>
+          </div>
+          <div className="actu4">
+            <div>
+              <h1 className="title-actu">{translations.title_actu4 || null}</h1>
+              <p className="content-actu4">
+                {translations.content_actu4 || null}
+              </p>
+              <p>
+                <a
+                  className="link-actu"
+                  href={externalLinks?.link_actu4}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Lien vers actu4
+                </a>
+              </p>
+            </div>
+            <div>
+              <img
+                className="photo_actu"
+                src={actus?.photo_actu4?.src}
+                alt={actus?.photo_actu4?.alt}
+              />
+            </div>
+          </div>
+          <div className="actu5">
+            <div>
+              <h1 className="title-actu">{translations.title_actu5 || null}</h1>
+              <p className="content-actu5">
+                {translations.content_actu5 || null}
+              </p>
+              <p>
+                <a
+                  className="link-actu"
+                  href={externalLinks?.link_actu5}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Lien vers actu5
+                </a>
+              </p>
+            </div>
+            <div>
+              <img
+                className="photo_actu"
+                src={actus?.photo_actu5?.src}
+                alt={actus?.photo_actu5?.alt}
+              />
+            </div>
           </div>
         </div>
-        <div className="actu2">
-          <div>
-            <h1 className="title-actu2">{translations.title_actu2 || null}</h1>
-            <p className="content-actu2">
-              {translations.content_actu2 || null}
-            </p>
-            <p>
-              <a
-                className="link-actu2"
-                href={externeLinks?.link_actu2}
-                target="_blank"
-                rel="noreferrer"
-              >
-                Lien vers actu2
-              </a>
-            </p>
+        <div className="twoActus-twit">
+          <h1 className="twit-title">Twitter widget</h1>
+          <div className="twit-content">
+            <a
+              className="twitter-timeline"
+              data-width="100vw"
+              data-dnt="true"
+              href={externalLinks?.link_twittier}
+            >
+              Tweets by edtech_hdf
+            </a>
+            <script async src={externalLinks?.link_widget} charset="utf-8">
+              Twitter Hautes de France
+            </script>
           </div>
-          <div>
-            <img
-              className="photo_actu"
-              src={images?.photo_actu2?.src}
-              alt={images?.photo_actu2?.alt}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="actu-widget-twitter">
-        <h2>Twitter widget</h2>
-        <div>
-          <a
-            className="twitter-timeline"
-            data-width="600px"
-            data-height="500px"
-            data-dnt="true"
-            href={externeLinks?.link_twittier}
-          >
-            Tweets by edtech_hdf
-          </a>
-          <script async src={externeLinks?.link_widget} charset="utf-8">
-            Twitter Hautes de France
-          </script>
         </div>
       </div>
     </SActuTwit>
--- src/components/HdfTwitter/Style.jsx
+++ src/components/HdfTwitter/Style.jsx
@@ -1,24 +1,47 @@
 import styled from 'styled-components';
 
 const SActuTwit = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  height: 600px;
-  margin-top: 3rem;
+  background-color: #2c2875;
+  color: #f5f5f5;
+
+  h1 {
+    padding: 1rem;
+    font-size: 2.5rem;
+    font-weight: 700;
+    text-align: center;
+  }
+
+  .ActuTwit {
+    display: flex;
+  }
+
+  .twoActus-twit {
+    position: relative;
+    padding: 1.5rem;
+    overflow-y: hidden;
+    width: 100%;
+  }
 
   .twoActus {
     line-height: 1.7;
+    font-size: 1.3rem;
+  }
+
+  .twoActus h1 {
+    font-size: 1.6rem;
+    font-weight: 700;
+    text-align: center;
   }
 
   .actu1 {
     display: flex;
-    margin-bottom: 2rem;
   }
 
   .photo_actu {
     margin-left: 1rem;
-    height: 15rem;
-    width: 15rem;
+    margin-top: 4.6rem;
+    height: 10rem;
+    width: 10rem;
     border-radius: 10px;
   }
 
@@ -26,8 +49,33 @@ const SActuTwit = styled.div`
     display: flex;
   }
 
-  .actu-widget-twitter {
+  .twoActus a {
+    text-decoration: none;
+    color: #f5f5f5;
+  }
+
+  .twit-title {
     text-align: center;
+    font-size: 1.6rem;
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
+  .twit-content {
+    position: absolute;
+    height: 100%;
+    width: 95%;
+    overflow-y: scroll;
+    border-radius: 10px;
+  }
+
+  .actu3 {
+    display: none;
+  }
+  .actu4 {
+    display: none;
+  }
+  .actu5 {
+    display: none;
   }
 `;
 
