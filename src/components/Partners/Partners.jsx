9403b8aa829cc1418625f35104475ded23107980
Georges FORTRY <gfortry@hotmail.com>
2021-07-01 17:33:15 +0200

    partners & members responsive

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import dotenv from 'dotenv';
+import Carousel from 'react-multi-carousel';
 import SPartners from './Style';
 
 dotenv.config();
@@ -9,6 +10,7 @@ export default function Partners() {
   const [partners, setPartners] = useState([]);
   const [translations, setTranslations] = useState({});
   const [colors, setColors] = useState({});
+  const [settings, setSettings] = useState({});
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API_URL}/partners`).then(({ data }) => {
@@ -20,6 +22,11 @@ export default function Partners() {
     axios.get(`${process.env.REACT_APP_API_URL}/colors`).then(({ data }) => {
       setColors(data);
     });
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/settings_carousel`)
+      .then(({ data }) => {
+        setSettings(data);
+      });
   }, []);
   return (
     <SPartners
@@ -42,6 +49,66 @@ export default function Partners() {
           );
         })}
       </div>
+      {settings.carousel_duration && (
+        <Carousel
+          additionalTransfrom={0}
+          arrows
+          autoPlay
+          autoPlaySpeed={parseInt(settings.carousel_duration, 10)}
+          centerMode={false}
+          className="Carousel"
+          containerClass="container"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024,
+              },
+              items: parseInt(settings.items_number, 10),
+              partialVisibilityGutter: 40,
+            },
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0,
+              },
+              items: 1,
+              partialVisibilityGutter: 30,
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464,
+              },
+              items: 2,
+              partialVisibilityGutter: 30,
+            },
+          }}
+          showDots
+          sliderClass=""
+          slidesToSlide={4}
+          swipeable
+        >
+          {partners.map((partner) => {
+            return (
+              <div key={partner.id} className="slide">
+                <a href={partner.link} target="_blank" rel="noreferrer">
+                  <img src={partner.logo_src} alt={partner.logo_alt} />
+                </a>
+              </div>
+            );
+          })}
+        </Carousel>
+      )}
     </SPartners>
   );
 }
