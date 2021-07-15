8f2e0d90aec068987b9aa9e9b4a1ba498da1342a
Hacene010 <hamrounehacene06@gmail.com>
2021-07-15 16:35:30 +0200

    latest changes

--- src/components/HdfMap/Style.jsx
+++ src/components/HdfMap/Style.jsx
@@ -6,7 +6,7 @@ const SMap = styled.div`
   padding-top: 30px;
   padding-bottom: 30px;
   color: ${(props) => props.text};
-  height: 350px;
+  height: 430px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -37,7 +37,7 @@ const SMap = styled.div`
       padding: 10px;
     }
     img {
-      max-width: 25%;
+      max-width: 40%;
       padding: 10px;
     }
   }
